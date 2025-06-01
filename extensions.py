ecaf1ee78086e67d37933f8b46ea46a60dac8b0a
ama122 <gackelornspe@gmail.com>
2025-06-01 02:49:46 -0700

    extention

---
+++ extensions.py
@@ -0,0 +1,21 @@
+user_file=input("file name:").lower().strip()
+if user_file.endswith(".gif"):
+    print("image/gif")
+
+elif user_file.endswith(".jpeg")or user_file.endswith(".jpg"):
+    print("image/jpeg")
+
+elif user_file.endswith(".txt"):
+    print("text/plain")
+
+elif user_file.endswith(".zip"):
+    print("application/zip")
+
+elif user_file.endswith(".pdf"):
+    print("application/pdf")
+
+elif user_file.endswith(".png"):
+    print("image/png")
+
+else:
+    print("application/octet-streem")
