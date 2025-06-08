a189e33b09b24e55479c13cd8c6d029b76066323
ama122 <gackelornspe@gmail.com>
2025-06-08 06:28:12 -0700

    deep

---
+++ deep.py
@@ -0,0 +1,7 @@
+true_word=["42","forty-tow","forty tow"]
+user=input("what is answer great qeautiion life,the universe, and evry thing?")
+if user in true_word :
+    print("Yes")
+else:
+    print("No")
+    
